6b3760ef09df7af4951cef6365cd4a570650bbe6
mohamamd <mohammad.hosein3135@gmail.com>
2024-02-27 09:53:32 +0330

    work on landing page

--- dist/js/main.js
+++ dist/js/main.js
@@ -6,24 +6,19 @@ hamburgerBtn.addEventListener('click', () => {
     mobileMenuEl.classList.toggle('open-menu');
 })
 
-// swiper 
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    loop: true,
+// fixed header 
 
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-    },
+const header = document.querySelector('#header');
+console.log(header);
 
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
+document.addEventListener('scroll', () => {
+    let scroll = scrollY;
+    console.log(scroll);
+    if (scroll > 150) {
+        header.classList.add('fixed')
+    }
+    if (scroll < 150) {
+        header.classList.remove('fixed')
 
-    // And if we need scrollbar
-    scrollbar: {
-        el: '.swiper-scrollbar',
-    },
-});
+    }
+})
