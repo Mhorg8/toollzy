79c28fdd930fa4707a7a0adb2d2849db42505db5
mohamamd <mohammad.hosein3135@gmail.com>
2024-03-04 23:49:41 +0330

    create login form

--- dist/js/main.js
+++ dist/js/main.js
@@ -34,3 +34,75 @@ toolBoxesEl.forEach((box, i) => {
         toolDescribtionEl[i].classList.remove('show-description')
     })
 })
+
+// handle flip timer
+
+
+const timer = document.querySelector('.timer');
+
+let minCount = 25;
+let count = 60;
+
+const appendZero = (val) => {
+    return val < 10 ? `0${val} ` : val
+};
+
+setInterval(() => {
+
+    if (minCount > 0) {
+        if (count !== 0) {
+            count--;
+        } else {
+            minCount--;
+            count = 60;
+        }
+        minCount < 3 ? timer.classList.add('opacity') : timer.classList.remove('opacity');
+    }
+
+    timer.textContent = `${appendZero(minCount)} : ${appendZero(count)}`;
+}, 1000);
+
+// login handler
+const emailInput = document.querySelector('#email-input');
+const passwordInput = document.querySelector('#password-input');
+const submitFormBtn = document.querySelector('#submit-btn');
+const loginForm = document.querySelector('#login-form');
+const loginBtn = document.querySelector('#login-btn');
+const loginContainer = document.querySelector('#login-page')
+const users = [];
+
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+});
+
+loginBtn.addEventListener('click', () => {
+    loginContainer.classList.toggle('show')
+})
+
+submitFormBtn.addEventListener('click', () => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+    const userObj = {};
+    checkValidation(emailRegex, passwordRegex, userObj);
+});
+
+function setLocal(users) {
+    localStorage.setItem("users", JSON.stringify(users));
+}
+
+function checkValidation(emailRegex, passwordRegex, userObj) {
+    if (emailInput.value && passwordInput.value) {
+        if (emailRegex.test(emailInput.value) && passwordRegex.test(passwordInput.value)) {
+            userObj.userEmail = emailInput.value;
+            userObj.userPassword = passwordInput.value;
+
+            users.push(userObj);
+            setLocal(users);
+            loginContainer.classList.remove('show')
+        } else {
+            alert('Email or password format is incorrect.');
+        }
+    } else {
+        alert('Please fill in all fields.');
+    }
+}
